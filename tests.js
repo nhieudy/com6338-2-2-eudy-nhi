4cf364580f2f42a84fd728a119fc54b9d20f04e5
Nhi Eudy <nhieudy@Mac.attlocal.net>
2025-08-29 14:54:06 -0500

    Alert for birth year

--- tests.js
+++ tests.js
@@ -191,14 +191,18 @@ function runTests() {
 
 //Greet Function
 function greet(){
-  //console.log('Test'); Just making sure the function is working with the button
-  //Ask for user's name:
   let userName = prompt("What is your name?");
-  //console.log(userName);
-  //Greet user with their name:
   alert("Hello, " + userName);
   let userAge = parseInt(prompt("How old are you?")); //Radix by default is 10
-  console.log(userAge);
-  confirm("Did you have a birthday yet this year?");
-
+  let userAnswer = confirm("Did you have a birthday yet this year?"); //True for OK and False for Cancel
+  let currentYear = new Date().getFullYear(); //Return vale: Int
+  let userYear = 0;
+  if(userAnswer){
+    userYear = currentYear - userAge;
+      alert("You were born in this year: " + userYear);
+  }
+  else{
+    userYear = (currentYear - (userAge+1));
+      alert("You were born in this year: " + userYear);
+  }
 }
