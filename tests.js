1b8318f473733d6e5a4bf24860aeb6d97e817c3d
Nhi Eudy <nhieudy@Mac.attlocal.net>
2025-09-10 15:49:40 -0500

    Moved my function from test.js to index.js

--- tests.js
+++ tests.js
@@ -188,21 +188,3 @@ function runTests() {
 
   mocha.run();
 }
-
-//Greet Function
-function greet(){
-  let userName = prompt("What is your name?");
-  alert("Hello, " + userName);
-  let userAge = parseInt(prompt("How old are you?")); //Radix by default is 10
-  let userAnswer = confirm("Did you have a birthday yet this year?"); //True for OK and False for Cancel
-  let currentYear = new Date().getFullYear(); //Return vale: Int
-  let userYear = 0;
-  if(userAnswer){
-    userYear = currentYear - userAge;
-      alert("You were born in this year: " + userYear);
-  }
-  else{
-    userYear = (currentYear - (userAge+1));
-      alert("You were born in this year: " + userYear);
-  }
-}
